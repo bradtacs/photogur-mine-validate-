291c2a147619db7b27af79c9ee4985ea2e95e896
Zachary Viggiani <zach@bigmojo.net>
2017-04-17 11:44:34 -0400

    validations added

---
+++ app/models/picture.rb
@@ -0,0 +1,8 @@
+class Picture < ApplicationRecord
+
+  validates :artist,  presence: true
+  validates :title, length: {in: 3..20}
+  validates :url, presence: true
+  validates :url, uniqueness: true
+
+end
