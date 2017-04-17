b04b21b763d2e9a0d5b4d93e38d9aaa8365b6b23
Zachary Viggiani <zach@bigmojo.net>
2017-04-17 18:40:59 -0400

    error view close

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,7 @@
 class Picture < ApplicationRecord
-  validates :artist,  presence: true
+  validates :artist,  presence: true # { message: "must be given please" }
   validates :title, length: {in: 3..20 }
-  validates :url, presence: true
+  validates :url, presence: true # { message: "must be given please" }
   validates :url, uniqueness: true
-
-
 
 end
