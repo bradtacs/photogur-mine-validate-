49f688e834d864d99340ea47340bf2afb610c228
Zachary Viggiani <zach@bigmojo.net>
2017-04-18 14:28:30 -0400

    form errors done maybe edit look later

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ApplicationRecord
-  validates :artist,  presence: true # { message: "must be given please" }
+  validates :artist,  presence: { message: "must be given please" }
   validates :title, length: {in: 3..20 }
-  validates :url, presence: true # { message: "must be given please" }
+  validates :url, presence: { message: "must be given please" }
   validates :url, uniqueness: true
 
 end
